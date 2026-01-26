f9278c0c7869592921a26bbd6ccca0809ae61466
triptishakya-dev <triptishakya.2001@gmail.com>
2026-01-27 01:40:42 +0530

    feat: Initialize full-stack application with Next.js frontend and Node.js/Express/Prisma backend, including user creation endpoint and PostgreSQL setup via Docker.

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import userRoutes from "./routes/userRoutes.js"
+
+
+const app= express();
+
+const port = 8000;
+
+
+app.use(express.json());
+
+app.use("/", userRoutes);
+
+
+
+ app.get("/" , (req,res) =>{
+    res.send("hello");
+ })
+app.listen (port, () => {
+    console.log("port is listening")
+ });
