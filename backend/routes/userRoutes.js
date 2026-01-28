57d58fcea36c5e4ec97c31f0af084f1e90e7e8e1
triptishakya-dev <triptishakya.2001@gmail.com>
2026-01-28 23:52:52 +0530

    feat: add user routes for POST and GET operations.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
 import postControllers from "../controllers/userControllers.js"
+import getuser from "../controllers/userControllers.js"
 
 const router = express.Router();
 
 router.post("/api/user", postControllers);
+router.get("/api/user" , getuser);
 
 export default router;
