f48dae01b87f72fcb51c4e4ffd18269a515cbf27
triptishakya-dev <triptishakya.2001@gmail.com>
2026-01-29 22:36:21 +0530

    Added rounded

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -22,7 +22,7 @@ export default function UserForm() {
     setMessage("");
 
     try {
-      setLoading(true)
+      setLoading(true);
       const res = await fetch("http://localhost:8000/api/user", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -35,7 +35,7 @@ export default function UserForm() {
 
       setMessage("User created successfully ✅");
       setForm({ name: "", email: "", password: "" });
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       setMessage(error.message || "Something went wrong ❌");
     } finally {
@@ -45,65 +45,70 @@ export default function UserForm() {
 
   return (
     <div className="h-screen w-full bg-white flex justify-between items-center">
-    <div className="max-w-md mx-auto mt-10 p-6 border rounded-xl shadow bg-gray-200 flex flex-col justify-center items-center">
-      <h2 className="text-2xl font-bold mb-4 text-black">Create User</h2>
-      <p className="text-black">Fill the form , i will be happy , thank you soo much</p>
+      <div className="max-w-md mx-auto mt-10 p-6 border rounded-2xl shadow bg-gray-200 flex flex-col justify-center items-center">
+        <h2 className="text-2xl font-bold mb-4 text-black">Create User</h2>
+        <p className="text-black">
+          Fill the form , i will be happy , thank you soo much
+        </p>
 
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="flex justify-center items-center gap-5 my-5 ">
-           <label htmlFor="name" className="text-black font-bold"> Name:-</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={form.name}
-          onChange={handleChange}
-          required
-          className="w-full p-2 border rounded text-black"
-        />
-        </div>
-       
-
-
-         <div className="flex justify-center items-center gap-5  ">
-           <label htmlFor="name" className="text-black font-bold"> Email:-</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={form.email}
-          onChange={handleChange}
-          required
-          className="w-full p-2 border border-black  text-black rounded"
-        />
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex justify-center items-center gap-5 my-5 ">
+            <label htmlFor="name" className="text-black font-bold">
+              {" "}
+              Name:-
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={form.name}
+              onChange={handleChange}
+              required
+              className="w-full p-2 border rounded text-black"
+            />
           </div>
-
-
-
-          3<div className="flex justify-center items-center gap-5  ">
-           <label htmlFor="name" className="text-black font-bold"> Password:-</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={form.password}
-          onChange={handleChange}
-          required
-          className="w-full p-2 border border-black text-black rounded"
-        />
+          <div className="flex justify-center items-center gap-5  ">
+            <label htmlFor="name" className="text-black font-bold">
+              {" "}
+              Email:-
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={form.email}
+              onChange={handleChange}
+              required
+              className="w-full p-2 border border-black  text-black rounded"
+            />
           </div>
+          3
+          <div className="flex justify-center items-center gap-5  ">
+            <label htmlFor="name" className="text-black font-bold">
+              {" "}
+              Password:-
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleChange}
+              required
+              className="w-full p-2 border border-black text-black rounded"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full bg-black text-white py-2 rounded"
+          >
+            {loading ? "Creating..." : "Create User"}
+          </button>
+        </form>
 
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full bg-black text-white py-2 rounded"
-        >
-          {loading ? "Creating..." : "Create User"}
-        </button>
-      </form>
-
-      {message && <p className="mt-4 text-center">{message}</p>}
-    </div>
+        {message && <p className="mt-4 text-center">{message}</p>}
+      </div>
     </div>
   );
 }
